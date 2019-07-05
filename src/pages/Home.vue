8ad62eec11a172af99854f44594ceae355f44e44
zhanglong11 <you@example.com>
2019-07-05 11:04:26 +0800

    first commit

---
+++ src/pages/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="home-wrapper">
+        home
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home"
+    }
+</script>
+
+<style scoped>
+    .home-wrapper{
+        color:red;
+    }
+</style>
