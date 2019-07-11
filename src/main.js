7897d07e0dffc99b1b67ccadd59e58c414fd01ed
zhanglong11 <you@example.com>
2019-07-11 15:19:56 +0800

    dist

--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './strore'
+import store from './strore';
+import I18n from 'vue-i18n';
 
 Vue.config.productionTip = false;
+Vue.use(I18n);
 
 
 new Vue({
     el: '#app',
     store,
+    I18n,
     router,  // 注入到根实例中
     render: h => h(App)
 })
